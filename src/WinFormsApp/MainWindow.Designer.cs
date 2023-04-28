e0336dfeebab64e0f5826179fe9ffa8bc170858b
Brian Lyttle <brianly@users.noreply.github.com>
2023-04-28 16:16:37 -0400

    Adding High DPI support

--- src/WinFormsApp/MainWindow.Designer.cs
+++ src/WinFormsApp/MainWindow.Designer.cs
@@ -135,8 +135,8 @@ namespace NMARC
             // 
             // FrmNativeModeConc
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(803, 521);
             this.Controls.Add(this.btnOptions);
             this.Controls.Add(this.btnConvert);
